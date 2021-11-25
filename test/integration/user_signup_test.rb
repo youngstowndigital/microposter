b219312a8038fae06c1ffbec453591481a205285
Mike Wittenauer <mikewittenauer@me.com>
2021-11-24 20:59:07 -0500

    Finish user signup

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -10,4 +10,18 @@ class UserSignupTest < ActionDispatch::IntegrationTest
     assert_select "div#error_explanation"
     assert_select "div.alert-danger"
   end
+
+  test "valid signup" do
+    get signup_path
+    assert_difference "User.count" do
+      post users_path, params: {
+        user: {
+          name: "Mike", email: "mike@me.com", password: "foobar", password_confirmation: "foobar"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end

e971506507bca2e4ebd4d20d7fadcf309c8b2007
Mike Wittenauer <mikewittenauer@me.com>
2021-11-25 22:14:27 -0500

    Test failed login path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash.now[:danger] = "Invalid credentials"
+      render 'new'
+    end
+  end
 end

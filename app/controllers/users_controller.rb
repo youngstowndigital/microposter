41cf11ccb5b42e334e601a2c56fe4688b2c20026
Mike Wittenauer <mikewittenauer@me.com>
2021-11-28 15:41:12 -0500

    Edit functionality without authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:success] = "Profiled updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
